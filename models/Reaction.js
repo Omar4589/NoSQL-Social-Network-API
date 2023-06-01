1cf547d96d295f6983dc750639f66634daf9f350
Omar4589 <omar4589@hotmail.com>
2023-06-01 01:37:09 -0500

    creates Reaction schema

---
+++ models/Reaction.js
@@ -0,0 +1,24 @@
+const { Schema, Types } = require('mongoose');
+
+const reactionSchema = new Schema({
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: () => new mongoose.Types.ObjectId()
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: dayjs(),
+    get: (somedate) => dayjs(somedate).format('MM/DD/YYYY')
+  }
+});
+
+module.exports = reactionSchema;
