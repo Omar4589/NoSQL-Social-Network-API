771d99bc036dca7ce121699cd9a1a348d8c29dc6
Omar4589 <omar4589@hotmail.com>
2023-06-01 20:19:34 -0500

    creates data

---
+++ utils/data.js
@@ -0,0 +1,26 @@
+// utils/data.js
+
+const data = {
+  users: [
+    {
+      username: "omarz17",
+      email: "omar@omar.com",
+    },
+    {
+      username: "johnDoe",
+      email: "johndoe@johndoe.com",
+    },
+  ],
+  thoughts: [
+    {
+      thoughtText: "Here's a cool thought...!",
+      username: "omarz17",
+    },
+    {
+      thoughtText: "What an incredible thought...!!!",
+      username: "johnDoe",
+    },
+  ],
+};
+
+module.exports = data;
