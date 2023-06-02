3549a5156bd5fb67d7f4f391f904fc158ec799ff
Omar4589 <omar4589@hotmail.com>
2023-06-01 20:28:33 -0500

    fixes thought model

--- models/Thought.js
+++ models/Thought.js
@@ -27,6 +27,6 @@ thoughtSchema.virtual("reactionCount").get(function () {
   return this.reactions.length;
 });
 
-const Thought = mongoose.model("thought", thoughtSchema);
+const Thought = model("thought", thoughtSchema);
 
 module.exports = Thought;
