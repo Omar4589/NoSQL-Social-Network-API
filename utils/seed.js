93a163cb3f69c5630af108e56ef8c83dc54c977e
Omar4589 <omar4589@hotmail.com>
2023-06-01 23:28:04 -0500

    updates seed file

--- utils/seed.js
+++ utils/seed.js
@@ -1,4 +1,3 @@
-// utils/seed.js
 const connection = require("../config/connection");
 const { User, Thought } = require("../models");
 const data = require("./data");
@@ -13,17 +12,20 @@ connection.once("open", async () => {
     await Thought.deleteMany({});
 
     const users = await User.insertMany(data.users);
+
     for (let thought of data.thoughts) {
       const { username } = thought;
       const user = users.find(user => user.username === username);
       if (user) {
-        thought.userId = user._id;
+        const createdThought = await Thought.create(thought);
+        await User.findOneAndUpdate(
+          { username: user.username },
+          { $push: { thoughts: createdThought._id } },
+          { runValidators: true, new: true }
+        );
       }
     }
 
-    const thoughts = await Thought.insertMany(data.thoughts);
-
-    console.log(users, thoughts);
     console.log("all done!");
     process.exit(0);
   } catch (err) {
